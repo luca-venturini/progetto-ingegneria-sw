dec6cf977e49b1f46c7b050f4622c7336606fa36
DomenicoParente <domenicoparente@mail.polimi.it>
2020-03-31 18:50:49 +0200

    implemented challenger

--- src/main/java/it/polimi/ingsw/ParenteVenturini/Model/Player.java
+++ src/main/java/it/polimi/ingsw/ParenteVenturini/Model/Player.java
@@ -1,11 +1,17 @@
 package it.polimi.ingsw.ParenteVenturini.Model;
 
+import it.polimi.ingsw.ParenteVenturini.Model.Board;
 import it.polimi.ingsw.ParenteVenturini.Model.Cards.Card;
+import it.polimi.ingsw.ParenteVenturini.Model.Checks.BasicWinCheck;
+import it.polimi.ingsw.ParenteVenturini.Model.Checks.WinCheck;
 import it.polimi.ingsw.ParenteVenturini.Model.Exceptions.AlreadyChosenStarter;
 import it.polimi.ingsw.ParenteVenturini.Model.Exceptions.InvalidCardException;
 import it.polimi.ingsw.ParenteVenturini.Model.Exceptions.InvalidNamePlayerException;
 import it.polimi.ingsw.ParenteVenturini.Model.Exceptions.NoChallengerException;
+import it.polimi.ingsw.ParenteVenturini.Model.Match;
 import it.polimi.ingsw.ParenteVenturini.Model.Moves.Move;
+import it.polimi.ingsw.ParenteVenturini.Model.Point;
+import it.polimi.ingsw.ParenteVenturini.Model.Worker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +21,32 @@ public class Player {
     private Card card;
     private List<Worker> workers;
     private String nickname;
+    private WinCheck winCondition;
     private Match match;
     private boolean challenger;
 
+
     public Player(String nickname, Match match) {
         this.nickname = nickname;
+        workers = new ArrayList<Worker>();
         this.match= match;
         this.challenger= false;
-        workers = new ArrayList<Worker>();
     }
 
     public void setCard(Card card){
         this.card = card;
+        setWinCondition();
+    }
+
+    private void setWinCondition(){
+        if(card == null || card.getWinCheck() == null)
+            this.winCondition = new BasicWinCheck();
+        else
+            this.winCondition = card.getWinCheck();
     }
 
     public void placeWorker(int colour , Point point, Board board){
-        Worker worker= new Worker(point,this,colour);
+        Worker worker= new Worker(point, this,colour);
         addWorker(worker);
         board.setWorker(worker);
     }
@@ -47,16 +63,17 @@ public class Player {
         Move myMove= card.getMove();
     }
 
+    public boolean hasWon(Board board){
+        return winCondition.hasWon(board, workers);
+    }
+
+
     public Card getCard() {
         return card;
     }
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
-    }
-
-    public boolean isChallenger() {
-        return challenger;
     }
 
     public void setChallenger(boolean challenger) {
@@ -78,4 +95,5 @@ public class Player {
     public String getNickname() {
         return nickname;
     }
+
 }
