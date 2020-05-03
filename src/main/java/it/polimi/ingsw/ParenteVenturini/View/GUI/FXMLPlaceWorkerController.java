b08ead3185e4c55c26bb3d3cae1e26d92ea8ed9b
DomenicoParente <domenico.parente@mail.polimi.it>
2020-05-03 12:10:05 +0200

    some add

---
+++ src/main/java/it/polimi/ingsw/ParenteVenturini/View/GUI/FXMLPlaceWorkerController.java
@@ -0,0 +1,67 @@
+package it.polimi.ingsw.ParenteVenturini.View.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class FXMLPlaceWorkerController implements ViewController{
+    @FXML
+    private Button block_4_4;
+    @FXML
+    private Button block_0_0;
+    @FXML
+    private Button block_1_0;
+    @FXML
+    private Button block_2_0;
+    @FXML
+    private Button block_3_0;
+    @FXML
+    private Button block_4_0;
+    @FXML
+    private Button block_0_1;
+    @FXML
+    private Button block_1_1;
+    @FXML
+    private Button block_2_1;
+    @FXML
+    private Button block_3_1;
+    @FXML
+    private Button block_4_1;
+    @FXML
+    private Button block_0_2;
+    @FXML
+    private Button block_1_2;
+    @FXML
+    private Button block_2_2;
+    @FXML
+    private Button block_3_2;
+    @FXML
+    private Button block_4_2;
+    @FXML
+    private Button block_0_3;
+    @FXML
+    private Button block_1_3;
+    @FXML
+    private Button block_2_3;
+    @FXML
+    private Button block_3_3;
+    @FXML
+    private Button block_4_3;
+    @FXML
+    private Button block_0_4;
+    @FXML
+    private Button block_1_4;
+    @FXML
+    private Button block_2_4;
+    @FXML
+    private Button block_3_4;
+
+    @Override
+    public void displayMessage(String s) {
+
+    }
+
+    @Override
+    public void enableButton() {
+
+    }
+}
